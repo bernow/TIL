fcff55ce3a64d67d9012fcc313d48cf993486963
bernow <50938270+bernow@users.noreply.github.com>
2019-07-20 18:03:08 +0900

    190720

---
+++ VO/UsersVO.java
@@ -0,0 +1,40 @@
+package vo;
+
+public class UsersVO {
+	private int user_no;
+	private String email;
+	private String password;
+	private String nickname;
+	private String admin;
+	
+	public int getUser_no() {
+		return user_no;
+	}
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getAdmin() {
+		return admin;
+	}
+	public void setAdmin(String admin) {
+		this.admin = admin;
+	}
+}
